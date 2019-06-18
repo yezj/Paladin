540adfbebfec322b4554bb33ed10eebcd95c2389
yezijian <2851827398@qq.com>
2019-06-18 09:21:22 +0800

    gm tool

--- src/front/debug/gm_tool.py
+++ src/front/debug/gm_tool.py
@@ -47,26 +47,32 @@ class EditProdHandler(ApiHandler):
     @api('Gm add prod', '/gm/add/prod/', [
         Param('user_id', True, str, '1', '1', 'user_id'),
         Param('pid', True, str, '01014', '01014', 'pid'),
-        Param('num', True, str, '1', '1', 'num'),
+        Param('num', False, str, '1', '1', 'num'),
+        Param('seconds', False, int, '1', '1', 'seconds'),
     ], filters=[ps_filter], description="Gm add prod")
     def get(self):
         try:
             user_id = self.get_argument("user_id")
             pid = self.get_argument("pid")
-            num = int(self.get_argument("num", 1))
         except Exception:
             raise web.HTTPError(400, "Argument error")
-
         user = yield self.get_player(user_id)
-        if num > 0:
-            if pid in user['props']:
-                user['props'][pid] += num
-            else:
-                user['props'][pid] = num
+        if 'l' in pid.split('_'):
+            seconds = self.get_argument("seconds")
+            expire_time = int(time.time()) + int(seconds)
+            user['props'][pid] = expire_time
+            msg = "SUCCESS! pid: " + pid + " expire_time:" + str(expire_time)
         else:
-            if pid in user['props']:
-                del user['props'][pid]
+            num = int(self.get_argument("num", 1))
+            if num > 0:
+                if pid in user['props']:
+                    user['props'][pid] += num
+                else:
+                    user['props'][pid] = num
+            else:
+                if pid in user['props']:
+                    del user['props'][pid]
+            msg = "SUCCESS! pid: " + pid + " curr num:" + str(num)
         cuser = dict(props=user['props'])
         yield self.set_player(user_id, **cuser)
-        msg = "SUCCESS! pid: " + pid + " curr num:" + str(num)
         self.write(msg)
